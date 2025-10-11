bc29d0d5167c6e9e5bd35b3719ddcd08bc1569f1
gugasmhm <gustavo.habbema@gmail.com>
2025-10-11 12:01:33 -0300

    Add files via upload

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,15 @@ import os
 def limpar_tela():
     """Limpa a tela (compatível com Windows, Linux e macOS)"""
     os.system("cls" if os.name == "nt" else "clear")
+
+import re
+
+# ------------------- Validador de Matrícula -------------------
+def validar_matricula():
+    padrao = re.compile(r"^\d{12}$")  # exatamente 12 dígitos
+    while True:
+        matricula = input("Digite a matrícula (12 dígitos): ").strip()
+        if padrao.match(matricula):
+            return matricula
+        else:
+            print("Matrícula inválida! Digite exatamente 12 números.\n")
